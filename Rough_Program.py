ea131af1b87e079d1b51966cce13988cbaa7833e
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-14 18:59:44 +0530

    Added rough file

---
+++ Rough_Program.py
@@ -0,0 +1,54 @@
+# type conversion
+# float() bool() str() int() 
+
+# name = input("Enter your name")
+# age = int(input("Enter your age"))
+# if age > 18:
+#     print("Eligible")
+# else :
+#     print("Not Eligible")
+
+
+# Range function returns a range object that is sequence of numbers
+
+# loop
+# for i in range(2,5):
+#     print(i)
+
+# list - mutable []
+# names = ["kaif","sid","som","anas"]
+# print(names)
+# for name in names:
+#     print(name)
+
+# tuples - immutable ()
+# marks = (2,3,4,5,6,7,8)
+# for mark in marks:
+#     print(mark)
+
+# sets - collection of all unique items {}
+# if in case I make a dublicate element then it is gonna remove it 
+
+# uniques = {1,2,3,4,5,5}
+# for unique in uniques:
+#     print(unique)
+
+# dictionary - unordered collection of items. Dictionary stores a (key,value) pair.
+# info = {"siddharth":21,"som":22,"kaif":23}
+# print(info["siddharth"])
+
+# Functions (def)
+# user defined function
+# def sum(a , b):
+#     print(a+b)
+# sum(5,4)
+
+#in-built functions
+# int() str() float() min() range() max()
+
+# module function 
+# modules is a file that contains some functions & variables which can be imported
+# import math
+# print(math.sqrt(25))
+
+# import random , import string 
