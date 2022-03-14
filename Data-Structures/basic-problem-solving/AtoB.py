b9274599416c5463da4817e16d666628da078165
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-14 19:05:12 +0530

    sovled a to b

---
+++ Data-Structures/basic-problem-solving/AtoB.py
@@ -0,0 +1,10 @@
+#Given 2 numbers,stored in variables with the following names min, max
+# To print all the numbers form min to max , excluing max .Print each number in a new line .
+
+def fromAtoB(A , B):
+    i = A 
+    while(i < B):
+        print(i)
+        i = i + 1
+        
+fromAtoB(13,19)
